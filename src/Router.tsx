3fe76f9a1e21eaf37a664b0dbb1e52e801e74b8a
Vinícius Berkembrock <berkembrock.vinicius@gmail.com>
2025-02-07 17:38:42 -0300

    feature/layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import { LayoutContainer } from "./layout/DefaultLayout/styles";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/issue/:idIssue/details" element={<Details />} />
+      <Route path="/" element={<LayoutContainer />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/issue/:idIssue/details" element={<Details />} />
+      </Route>
     </Routes>
   );
 }
